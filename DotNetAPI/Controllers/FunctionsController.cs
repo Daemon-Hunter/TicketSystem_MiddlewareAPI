491680e263dc1573e873a1858a967bcc91b55e83
Joshua Kellaway <dennis358.jk@gmail.com>
2016-04-17 16:41:57 +0100

    Added get[Table]Amount() where Table = {ARTIST, PARENT_EVENT, VENUE} to allow the client app to request a specific amount of objects.

--- DotNetAPI/Controllers/FunctionsController.cs
+++ DotNetAPI/Controllers/FunctionsController.cs
@@ -18,5 +18,89 @@ namespace DotNetAPI.Controllers
         {
             return db.COMPAREPASSWORDS(id, password);
         }
+
+        [HttpGet]
+        [Route("api/functions/getartistsamount/{amount}/{lowestID}")]
+        public List<ARTIST> getArtistsAmount(int amount, int lowestID)
+        {
+            List<ARTIST> artistList = new List<ARTIST>();
+            ARTIST artist;
+            int count = 0;
+            int lastID = db.ARTISTs.AsEnumerable().Last().ARTIST_ID;
+            int diff = 0;
+
+            if (lowestID != 0)
+            {
+                diff = lastID - lowestID + 1;
+            }
+
+            for (int id = lastID - diff; count != amount && id >= 0; id--)
+            {
+                artist = db.ARTISTs.Find(id);
+                if (artist != null)
+                {
+                    artistList.Add(artist);
+                    count++;
+                }
+            }
+
+            return artistList;
+        }
+
+        [HttpGet]
+        [Route("api/functions/getvenuesamount/{amount}/{lowestID}")]
+        public List<VENUE> getVenuesAmount(int amount, int lowestID)
+        {
+            List<VENUE> venueList = new List<VENUE>();
+            VENUE venue;
+            int count = 0;
+            int lastID = db.VENUEs.AsEnumerable().Last().VENUE_ID;
+            int diff = 0;
+
+            if (lowestID != 0)
+            {
+                diff = lastID - lowestID + 1;
+            }
+
+            for (int id = lastID - diff; count != amount && id >= 0; id--)
+            {
+                venue = db.VENUEs.Find(id);
+                if (venue != null)
+                {
+                    venueList.Add(venue);
+                    count++;
+                }
+            }
+
+            return venueList;
+        }
+
+        [HttpGet]
+        [Route("api/functions/getparenteventsamount/{amount}/{lowestID}")]
+        public List<PARENT_EVENT> getParentEventsAmount(int amount, int lowestID)
+        {
+            List<PARENT_EVENT> parentEventList = new List<PARENT_EVENT>();
+            PARENT_EVENT parentEvent;
+            int count = 0;
+            int lastID = db.PARENT_EVENT.AsEnumerable().Last().PARENT_EVENT_ID;
+            int diff = 0;
+
+            if (lowestID != 0)
+            {
+                diff = lastID - lowestID + 1;
+            }
+
+            for (int id = lastID - diff; count != amount && id >= 0; id--)
+            {
+                parentEvent = db.PARENT_EVENT.Find(id);
+                if (parentEvent != null)
+                {
+                    parentEventList.Add(parentEvent);
+                    count++;
+                }
+            }
+
+            return parentEventList;
+        }
     }
 }
